d5bf34f46ba562ddc7efc374fb0fc5076f23da68
Brehanu Bugg <buggbaj@gmail.com>
2021-02-09 20:18:40 -0500

    boilerplate complete

---
+++ README.md
@@ -0,0 +1,11 @@
+# Spark Camp Registration
+
+A public-facing homepage and registration portal for Spark Camp '21
+
+**Created by Charlie Hall and Brehanu Bugg**
+
+---
+
+## Installation
+
+Run `npm install` to download dependencies and then run `npm start` to expose the server. *Note that the server automatically restarts whenever a file is saved.*
---
+++ server.js
@@ -0,0 +1,29 @@
+// import required dependencies
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+
+// application setup
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine(".hbs", exphbs({extname: ".hbs"}));
+app.set("view engine", ".hbs");
+app.set("views", `${__dirname}/views`);
+
+// summer camp year
+const get_year = () => new Date().getFullYear().toString().substr(-2);
+
+// application routes
+app.get("/", (req, res) => {
+    res.render("index", {
+        "title": `Spark Camp '${get_year()}`
+    });
+});
+
+// start application
+app.listen(8080);
