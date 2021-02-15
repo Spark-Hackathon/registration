7efbf2b73111856f925ff11d79dd05c9eadf3a50
charlie-map <charjhall777@gmail.com>
2021-02-15 12:47:26 -0500

    wrong file

--- db_setup/TEST_DATA.sql
+++ db_setup/TEST_DATA.sql
@@ -6,6 +6,10 @@ INSERT INTO week (title, start_date, end_date, cb_code, inClass_available, virtu
 INSERT INTO week (title, start_date, end_date, cb_code, inClass_available, virtual_available) VALUES ("Creative Coding", "2021-07-12", "2021-07-16", "RANDOM_CODE2", 1, 1);
 INSERT INTO week (title, start_date, end_date, cb_code, inClass_available, virtual_available) VALUES ("Art of Games", "2021-07-19", "2021-07-23", "RANDOM_CODE3", 1, 1);
 
+INSERT INTO question_meta (week_id, question_text) VALUES (1, "Why do you think web should be an important component of school life?");
+INSERT INTO question_meta (week_id, question_text) VALUES (2, "Do you think coding can be creative? Interpet");
+INSERT INTO question_meta (week_id, question_text) VALUES (3, "How do you think you might use art to provide a more wonderful game?");
+
 INSERT INTO camper (first_name, last_name, email, dob, school, grade, gender, type, race_ethnicity, hopes_dreams, tshirt_size, 
 	borrow_laptop, guardian_name, guardian_email, guardian_phone, participated) VALUES ("Charlie", "Hall", "chall22@students.stab.org",
 	"2003-08-07", "St. Annes-Belfield", 11, "male", 5, "Caucasian", "I hope to learn more aboute Web Dev", "Xsmall", 0, "Kendra Hall",
@@ -21,6 +25,11 @@ INSERT INTO enrollment (camper_id, week_id, signup_time, enrollment_code, person
 INSERT INTO enrollment (camper_id, week_id, signup_time, enrollment_code, person_loc, approved) VALUES (1, 3, "2021-02-15 18:50:23.599", 
 	"SOME_UUID1", 1, 1);
 INSERT INTO enrollment (camper_id, week_id, signup_time, enrollment_code, person_loc, approved) VALUES (2, 2, "2021-02-17 08:50:50.823", 
-	"SOME_UUID2", 1, 1);
+	"SOME_UUID2", 1, 0);
 INSERT INTO enrollment (camper_id, week_id, signup_time, enrollment_code, person_loc, approved) VALUES (2, 3, "2021-02-17 08:50:50.823", 
-	"SOME_UUID2", 1, 1);
+	"SOME_UUID2", 1, 0);
+
+INSERT INTO questions (camper_id, question_meta_id, question_response) VALUES (1, 1, "Because it give students the correct amount of problem-solving and artistic pieces");
+INSERT INTO questions (camper_id, question_meta_id, question_response) VALUES (1, 3, "I think I'd use the art to provide more 'emotionally' telling stories");
+INSERT INTO questions (camper_id, question_meta_id, question_response) VALUES (2, 2, "Yeah, you can name variables whatever you want");
+INSERT INTO questions (camper_id, question_meta_id, question_response) VALUES (2, 3, "Because it makes it look cooler");
