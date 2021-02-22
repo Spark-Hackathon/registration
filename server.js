73dfbe9eec87e2738ea3325f9a97251080dd1519
charlie-map <charjhall777@gmail.com>
2021-02-21 23:19:31 -0500

    uh oh: spell check

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // import required dependencies
-require('dotenv').config({path: __diranme + "./env"});
+require('dotenv').config({path: __dirname + "./env"});
 const express = require("express");
 const app = express();
 
