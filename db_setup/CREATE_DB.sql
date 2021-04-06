5d67a3aa8ee02a37c88e6eb89060db21238a8a09
Charlie <charjhall777@gmail.com>
2021-04-06 18:26:19 -0400

    db changes

--- db_setup/CREATE_DB.sql
+++ db_setup/CREATE_DB.sql
@@ -51,8 +51,7 @@ CREATE TABLE prospect (
 	email VARCHAR(255) NOT NULL,
 	subscribed TINYINT(1) NOT NULL DEFAULT 1,
 	FOREIGN KEY (`camper_refer_id`) REFERENCES camper (`id`) ON DELETE CASCADE,
-	UNIQUE KEY `unique_prospect_email` (`email`),
-	UNIQUE KEY `unique_prospect_name` (`name`)
+	UNIQUE KEY `unique_prospect_email` (`email`)
 );
 CREATE TABLE question_meta (
 	id INT NOT NULL AUTO_INCREMENT,
